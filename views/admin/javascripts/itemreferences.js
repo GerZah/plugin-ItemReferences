2ea5dce64ae1096d7cb678437c13c14c74ecd805
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2016-07-05 18:59:18 +0200

    Back/"Front"ports from and to ItemRelations: 1. Limit Item ID filter field 2. Display Item IDs within list 3. Improved Layout / "Select Item" button visibility

--- views/admin/javascripts/itemreferences.js
+++ views/admin/javascripts/itemreferences.js
@@ -1,18 +1,28 @@
 jQuery(document).bind("omeka:elementformload", function() {
 //jQuery(document).ready(function () {
-
   var $ = jQuery; // use noConflict version of jQuery as the short $ within this block
-
   var options = {};
 
+  var updateTimer = null;
+
+  init();
+
+  function init() {
+      resetOptions();
+
+      $('new_relation_object_item_type_id_reference').val(-1);
+      $('input[name=itemsListsortReference]:checked').val('timestamp_reference');
+      $('#partial_object_title_reference').val('');
+      $('#id_limit_reference').val('');
+
+      updateChoices();
+      updateSelectButton();
+  }
+
   function resetOptions() {
-      options['partialReference'] = '';
-      options['item_typeReference'] = -1;
-      options['sortReference'] = 'mod_desc_ref';
-      options['pageReference'] = 0;
-      options['per_pageReference'] = 15;
       options = {
           partialReference: '',
+          id_limitReference: '',
           item_typeReference: -1,
           sortReference: 'mod_desc_ref',
           pageReference: 0,
@@ -21,11 +31,16 @@ jQuery(document).bind("omeka:elementformload", function() {
       };
   }
 
-  resetOptions();
-  updateChoices();
-
   function updateChoices() {
+    if (updateTimer != null) { clearTimeout(updateTimer); }
+    updateTimer = setTimeout(updateChoicesCore, 1000);
+  }
+
+  function updateChoicesCore() {
+      if (updateTimer != null) { clearTimeout(updateTimer); updateTimer = null; }
+
       options['partialReference'] = $('#partial_object_title_reference').val();
+      options['id_limitReference'] = $('#id_limit_reference').val();
       options['item_typeReference'] = $('#new_relation_object_item_type_id_reference').val();
       if ($('input[name=itemsListsortReference]:checked').val() === 'timestamp_reference') {
           options['sortReference'] = 'mod_desc_ref';
@@ -50,7 +65,9 @@ jQuery(document).bind("omeka:elementformload", function() {
               /* options */
               $('#lookup-results-reference').find('li').remove();
               for (i = 0; i < data['items'].length; ++i) {
-                  items.push('<li data-value="' + data['items'][i]['value'] + '">' + data['items'][i]['label'] + '</li>');
+                  items.push('<li data-value="' + data['items'][i]['value'] + '">' +
+                  '<span class="refListItemId">#' + data['items'][i]['value'] + "</span> " +
+                  data['items'][i]['label'] + '</li>');
               }
               $('#lookup-results-reference').append(items.join(''));
 
@@ -75,20 +92,39 @@ jQuery(document).bind("omeka:elementformload", function() {
   }
   $('#lookup-results-reference').on('click', 'li', function () {
       $('#new_reference_object_item_id_reference').val($(this).attr('data-value'));
-      $('#object_title_reference').html($(this).html());
+      $('#object_title_reference').html(
+        '<a href="' + $('#object_title_reference').attr('data-base-url') + '/items/show/' + $(this).attr('data-value') + '" target="_blank">' +
+        $(this).html() +
+        '</a>'
+      );
+      updateSelectButton();
   });
 
-  $('#selector-previous-page-reference').click(function () {
+  function updateSelectButton() {
+      var selectButton = $('#select_item');
+      if ($('#new_reference_object_item_id_reference').val()) {
+          selectButton.removeProp('disabled');
+          selectButton.removeAttr('disabled');
+      }
+      else {
+          selectButton.prop('disabled', true);
+          selectButton.attr('disabled', true);
+      }
+  }
+
+  $('#selector-previous-page-reference').click(function (e) {
+      e.preventDefault();
       if (0 < options['pageReference']) {
           options['pageReference']--;
-          updateChoices();
+          updateChoicesCore();
       }
   });
 
-  $('#selector-next-page-reference').click(function () {
+  $('#selector-next-page-reference').click(function (e) {
+      e.preventDefault();
       if (options['pageReference'] < options['max_pageReference']) {
           options['pageReference']++;
-          updateChoices();
+          updateChoicesCore();
       }
   });
 
@@ -97,18 +133,20 @@ jQuery(document).bind("omeka:elementformload", function() {
   });
 
   $('#new_selectObjectsort_timestamp_reference').click(function () {
-      updateChoices();
+      updateChoicesCore();
   });
 
   $('#new_selectObjectsort_name_reference').click(function () {
-      updateChoices();
+      updateChoicesCore();
   });
 
   $('#partial_object_title_reference').on('input', function () {
       updateChoices();
   });
 
-
+  $('#id_limit_reference').on('input', function () {
+      updateChoices();
+  });
 
   var lightbox = lity(); // https://www.npmjs.com/package/lity
   //var selectButtonTxt = $(".itemReferencesBtn").first().text();
@@ -129,7 +167,11 @@ jQuery(document).bind("omeka:elementformload", function() {
 
     $("#select_item").unbind("click").click(function(e) {
       e.preventDefault();
-      $("#"+currentTitle).val($('#object_title_reference').text());
+
+      var currentTitleVerb = $('#object_title_reference').text();
+      currentTitleVerb = currentTitleVerb.substr(currentTitleVerb.indexOf(" ")+1);
+
+      $("#"+currentTitle).val(currentTitleVerb);
       $("#"+currentId).val($('#new_reference_object_item_id_reference').val());
 
       lightbox.close();
